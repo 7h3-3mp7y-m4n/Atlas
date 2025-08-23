85a49928c0216b3316916ff6a1d7f37e33e04c07
7h3-3mp7y-m4n <emailtorash@gmail.com>
2025-08-24 04:03:28 +0530

    add simple enqueue logic
    Signed-off-by: 7h3-3mp7y-m4n <emailtorash@gmail.com>

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.24.3
 require github.com/google/uuid v1.6.0
 
 require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
--- internal/queue/redis.go
+++ internal/queue/redis.go
@@ -1,3 +1,77 @@
 package queue
 
-//thinking to connect to redis !! 🤩
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/7h3-3mp7y-m4n/atlas/internal/config"
+	"github.com/7h3-3mp7y-m4n/atlas/internal/models"
+	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
+)
+
+type RedisQueue struct {
+	client *redis.Client
+	queue  string
+}
+
+type QueueMessage struct {
+	JobID     uuid.UUID       `json:"job_id"`
+	Type      models.JobType  `json:"type"`
+	Payload   json.RawMessage `json:"payload"`
+	Priority  int             `json:"priority"`
+	Retry     int             `json:"retry"`
+	MaxRetry  int             `json:"max_retry"`
+	CreatedAt time.Time       `json:"created_at"`
+}
+
+func NewRedisQueue(cfg *config.RedisConfig, queue string) (*RedisQueue, error) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:         cfg.RedisAddr(),
+		Password:     cfg.Password,
+		DB:           cfg.DB,
+		PoolSize:     cfg.PoolSize,
+		MinIdleConns: cfg.MinIdle,
+		MaxRetries:   cfg.MaxRetries,
+		DialTimeout:  cfg.DialTimeout,
+	})
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		return nil, fmt.Errorf("failed to connect to Redis: %w", err)
+	}
+	return &RedisQueue{
+		client: rdb,
+		queue:  queue,
+	}, nil
+}
+
+func (q *RedisQueue) Enqueue(ctx context.Context, job *models.Job) error {
+	message := QueueMessage{
+		JobID:     job.ID,
+		Type:      job.Type,
+		Payload:   job.Payload,
+		Priority:  job.Priority,
+		Retry:     job.RetryCount,
+		MaxRetry:  job.MaxRetries,
+		CreatedAt: time.Now().UTC(),
+	}
+	data, err := json.Marshal(message)
+	if err != nil {
+		return fmt.Errorf("failed to marshal job message: %w", err)
+	}
+	// Using priority based queuing
+	queueKey := q.getQueueKey()
+	if job.Priority >= models.PriorityHigh {
+		return q.client.LPush(ctx, queueKey, data).Err()
+	} else {
+		return q.client.RPush(ctx, queueKey, data).Err()
+	}
+
+}
+
+func (q *RedisQueue) getQueueKey() string {
+	return fmt.Sprintf("queue:%s", q.queue)
+}
